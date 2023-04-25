c66cdf3abe063bf109b2eb3cc8ef3b2cd0d0051f
SamueleCrispino <samuele.crispino94@gmail.com>
2023-04-25 15:02:22 +0200

    removed p_1 computation from while loop

--- progetto_bis.py
+++ progetto_bis.py
@@ -6,14 +6,16 @@ from numpy import linalg
 
 
 def compute_inverted_p(a, n):
-    if np.linalg.det(a) == 0:
-        # this check also allow as a safe division 1/a.diagonal()
-        raise Exception(f"A matrix has a null determinant: can't compute p_1 matrix")
-
     # init P
     p_1 = np.zeros((n, n))
 
+    a_diag = a.diagonal()
+
+    if 0 in a_diag:
+        raise Exception(f"P matrix has a null determinant, hence it's singular and can't compute p_1 matrix")
+
     np.fill_diagonal(p_1, 1/a.diagonal())
+    
     return p_1
 
 def compute_residue(a, x, b):
@@ -23,8 +25,32 @@ def compare_scaled_residue(r, b, tol):
     return linalg.norm(r) / linalg.norm(b) < tol
 
 
+def input_validation(a, b):
+    n = a.shape[0]
+    n1 = a.shape[1]
+    
+    # check a simmetry:
+    if not n == n1:
+        raise Exception(f"Non-symmetric a matrix, dimensions found {n}, {n1}") 
+
+    b_dim = b.shape[0]
+    
+    # comparing matrix and vector dimensions
+    if not b_dim == n1:
+        raise Exception(f"Non-comparable dimensions matrix a: {n},{n} and vector b: {b_dim}")
+
+    if not np.all(linalg.eigvals(a) > 0):
+        raise Exception(f"a matrix is not positive definite")
+
+    if linalg.det(a) == 0:
+        raise Exception(f"Negative determinant for a matrix: {a}")
+    
+
+    return n
+
+
 # generic iterative method:
-def generic_iterative_method(a, b, tol, stop='scaled_residue'):
+def generic_iterative_method(a, b, tol=0.0001, stop='scaled_residue'):
     # TODO: pay attention to /0 operations
 
     # get A matrix dimensions
@@ -36,6 +62,10 @@ def generic_iterative_method(a, b, tol, stop='scaled_residue'):
     x = np.zeros(n)
     max_iter = 2000 if n <= 2000 else n
 
+    # case b = null and assuming that determinant of a matrix is different from zero
+    if not np.any(b):
+        print("A null b vector is passed")
+        return x
 
     
     # do i actually need this if condition ??
@@ -46,10 +76,11 @@ def generic_iterative_method(a, b, tol, stop='scaled_residue'):
         # increment over two successive iterations
         pass
 
+    # computing P^-1
+    inverted_p = compute_inverted_p(a, n)
+    print("inverted_p = ", inverted_p)
+
     while k <= max_iter and not stop_check:
-        # computing P^-1
-        inverted_p = compute_inverted_p(a, n)
-        print("inverted_p = ", inverted_p)
 
         # computing residue
         r = compute_residue(a, x, b)
@@ -66,6 +97,7 @@ def generic_iterative_method(a, b, tol, stop='scaled_residue'):
         # computing stop check
         stop_check = compare_scaled_residue(r, b, tol)
 
+    # TODO: saving these for plot
     print(f"x = {x}")
     print(f"k = {k}")
     print(f"tol = {tol}")
@@ -79,6 +111,6 @@ def generic_iterative_method(a, b, tol, stop='scaled_residue'):
 a = np.array([5, 2, 3, 4]).reshape(2, 2)
 print(a)
 
-b = np.array([16, 18])
+b = np.array([30, 46])
 
-generic_iterative_method(a, b, 0.1, stop='scaled_residue')
+generic_iterative_method(a, b, stop='scaled_residue')
