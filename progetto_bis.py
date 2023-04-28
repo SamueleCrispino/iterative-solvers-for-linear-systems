c007e71dd8848d813a810e8335129b0551a55812
SamueleCrispino <samuele.crispino94@gmail.com>
2023-04-28 10:05:41 +0200

    improved

--- progetto_bis.py
+++ progetto_bis.py
@@ -1,28 +1,45 @@
 import numpy as np
-
+from tqdm import tqdm
 from scipy.io import mmread
+from scipy.sparse import csr_matrix, tril, coo_matrix
 from numpy import linalg
 
+def compute_rel_error(x, real_x):
+    return linalg.norm(x - real_x) / linalg.norm(real_x)
 
+def create_mock(a):    
+    real_x = np.ones(a.shape[0])
+    b = a.dot(real_x)    
+
+    return real_x, b
+
+def init_values(a, n):
+    k = 0
+    stop_check = False
+    x = np.zeros(n)
+    max_iter = 2000 if n <= 2000 else n
+
+    return k, stop_check, x, max_iter
 
 def compute_p(a, n, method):
 
     if method == 'jacobi':
-        p_1 = np.zeros((n, n))
+        p_1 = coo_matrix((n, n))
         a_diag = a.diagonal()
-        np.fill_diagonal(p_1, a_diag)
+        #np.fill_diagonal(p_1, a_diag)
+        p_1.setdiag(a_diag)
 
     elif method == 'GS':
-        p_1 = np.tril(a)  
+        p_1 = tril(a)  
 
     else:
         raise Exception(f"No options found for method: {method}")
 
     # check if is non-singular
-    if linalg.det(p_1) == 0:
-        raise Exception(f"Negative determinant for a matrix: {p_1}")
+    if linalg.det(p_1.toarray()) == 0:
+        raise Exception(f"Null determinant for a matrix: {p_1}")
 
-    return p_1
+    return p_1.tocsr()
 
 
 
@@ -61,12 +78,12 @@ def input_validation(a, b):
 
 def forward_substitution(l, r, n):
     y = np.zeros(n)
+
     if l[0, 0] == 0:
         raise Exception(f"input matrix l: {l} has zero values on diagonal")
     y[0] = r[0] / l[0, 0]
 
     for i in range(1, n):
-        print(i)
         if l[i, i] == 0:
             raise Exception(f"input matrix l: {l}  has zero values on diagonal")
 
@@ -76,8 +93,10 @@ def forward_substitution(l, r, n):
 
 
 # generic iterative method:
-def generic_iterative_method(a, b, method, tol=0.0001, validation=False):
+def generic_iterative_method(a, b, real_x, method, tol=0.0001, validation=False):
     # TODO: pay attention to /0 operations
+
+    print("Starting iterative method")
 
     if validation:
         n = input_validation(a, b)
@@ -85,11 +104,12 @@ def generic_iterative_method(a, b, method, tol=0.0001, validation=False):
         # get A matrix dimensions
         n = a.shape[0]
 
-    # init counter, stop_check, null vector, max_iter
-    k = 0
-    stop_check = False
-    x = np.zeros(n)
-    max_iter = 2000 if n <= 2000 else n
+
+    # init counter, stop_check, null vector, max_iter, real_X, b vector
+    k, stop_check, x, max_iter = init_values(a, n)
+    
+    print(f"tol = {tol}")
+    print(f"max_iter = {max_iter}")
 
     # case b = null and assuming that determinant of a matrix is different from zero
     if not np.any(b):
@@ -98,37 +118,60 @@ def generic_iterative_method(a, b, method, tol=0.0001, validation=False):
 
     p = compute_p(a, n, method)
 
-    while k <= max_iter and not stop_check:
+    with tqdm(total = max_iter) as pbar:
+        while k <= max_iter and not stop_check:
 
-        # computing residue
-        r = compute_residue(a, x, b)
-        print("r = ", r)
+            # computing residue
+            r = compute_residue(a, x, b)
+            #print("r = ", r)
 
-        # compunting new x
-        x = x - forward_substitution(p, r, n)
-        print("x = ", x)
+            # compunting new x
+            x = x - forward_substitution(p, r, n)
+            #print("x = ", x)
 
-        # increasing iterations counter
-        k = k + 1
-        print(k)
+            # increasing iterations counter
+            k = k + 1
+            #print(k)
 
-        # computing stop check
-        stop_check = compare_scaled_residue(r, b, tol)
+            # computing stop check
+            stop_check = compare_scaled_residue(r, b, tol)
+            
+            pbar.update(1)
 
     # TODO: saving these for plot
-    print(f"x = {x}")
-    print(f"k = {k}")
-    print(f"tol = {tol}")
+    
 
     if k > max_iter:
-        print(f"Exceeded max number of iterations: {k}")
+        print(f"Exceeded max number of iterations: {max_iter}")
+
+    print(f"x = {x}")
+    print(f"k = {k}")
+    err_rel = compute_rel_error(x, real_x)
+    print(f"err_rel = {err_rel}")
 
     return x
 
+def main():
+    a = mmread('data/vem1.mtx')
 
-a = np.array([5, 2, 3, 4]).reshape(2, 2)
-b = np.array([30, 46])
+    #a = coo_matrix(np.array([5, 2, 3, 4]).reshape(2, 2))
+    
+    real_x, b = create_mock(a)
 
-generic_iterative_method(a, b, 'GS', validation=True)
+    generic_iterative_method(a, b, real_x, 'GS', validation=False)
+
+
+
+if __name__ == "__main__":
+    main()
+
+#a = np.array([5, 2, 3, 4]).reshape(2, 2)
+#b = np.array([30, 46])
+
+
+
+
+
+
 
 
